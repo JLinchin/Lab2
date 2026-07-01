d616b9b45e9bddbba6fd3a2f7bebe5a3996ce96a
Joseph Minchin <joseph.minchin@ecole-hexagone.com>
2026-07-01 10:21:32 +0200

    Ajout de veleurs par défaut

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 import joblib
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+app = FastAPI(
+    title="API Housing",
+    description="Prédiction du prix médian d'un logement (California Housing)."
+)
 modele = joblib.load("model.joblib")
 
 class Features(BaseModel):
-    MedInc: float
-    HouseAge: float
-    AveRooms: float
-    AveBedrms: float
-    Population: float
-    AveOccup: float
-    Latitude: float
-    Longitude: float
+    MedInc: float = Field(default=8.3)
+    HouseAge: float = Field(default=41)
+    AveRooms: float = Field(default=6.9)
+    AveBedrms: float = Field(default=1)
+    Population: float = Field(default=322)
+    AveOccup: float = Field(default=2.5)
+    Latitude: float = Field(default=37.88)
+    Longitude: float = Field(default=-122.23)
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
 @app.post("/predict")
-def predict(features: Features):
+def predict(features: Features = Body(default_factory=Features)):
     X = [[
         features.MedInc,
         features.HouseAge,
